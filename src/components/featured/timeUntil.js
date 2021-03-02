22d9b29e189933cd3b97d58559d6c5051efde90d
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-03-02 19:11:25 -0300

    finishing the countdown

--- src/components/featured/timeUntil.js
+++ src/components/featured/timeUntil.js
@@ -2,7 +2,12 @@ import React,{ useState,useEffect,useCallback } from 'react';
 import { Slide } from 'react-awesome-reveal';
 
 const TimeUntil = () => {
-
+    const [time,setTime] = useState({
+        days:'0',
+        hours:'0',
+        minutes:'0',
+        second:'0'
+    })
 
     const renderItem = (time,value) => (
         <div className="countdown_item">
@@ -15,20 +20,31 @@ const TimeUntil = () => {
         </div>
     )
 
-    const getTimeUntil = (deadline) => {
+    const getTimeUntil = useCallback( (deadline) => {
         const time = Date.parse(deadline) - Date.parse(new Date());
         if(time < 0){
             console.log('Date passed')
         } else{
-            
-        }   
+            const seconds = Math.floor((time/1000)%60);
+            const minutes = Math.floor((time/1000/60)%60);
+            const hours = Math.floor((time/(1000*60*60))%24);
+            const days = Math.floor((time/(1000*60*60*24)));
 
-
-    }
+            setTime({
+                days,
+                hours,
+                minutes,
+                seconds
+            })
+        }  
+    },[])
+    
+    
+   
 
 
     useEffect(()=>{
-        setInterval(()=> getTimeUntil('Jan, 20, 2021, 01:20:00'),1000)
+        setInterval(()=> getTimeUntil('Dec, 20, 2021, 01:20:00'),1000)
     },[getTimeUntil])
 
 
@@ -39,13 +55,10 @@ const TimeUntil = () => {
                     Event starts in
                 </div>
                 <div className="countdown_bottom">
-                    {renderItem(27,'Days')}
-                    {renderItem(4,'HS')}
-                    {renderItem(10,'Min')}
-                    {renderItem(50,'Sec')}
-
-
-
+                    {renderItem(time.days,'Days')}
+                    {renderItem(time.hours,'HS')}
+                    {renderItem(time.minutes,'Min')}
+                    {renderItem(time.seconds,'Sec')}
                 </div>
             </div>
         </Slide>

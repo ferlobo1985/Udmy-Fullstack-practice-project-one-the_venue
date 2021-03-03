d76c5d3804ebf62cb572489305a6d72828c9d545
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-03-03 02:55:38 -0300

    Adding the gmaps component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Featured from './components/featured';
 import VenueNfo from './components/venueNfo';
 import Highlights from './components/highlights'
 import Pricing from './components/pricing';
+import Location from './components/location';
 
 const App = () => {
   return (
@@ -30,10 +31,9 @@ const App = () => {
         <Pricing/>
       </>
 
-      <div style={{backgroundColor:'red',height:'800px'}}></div>
-      <div style={{backgroundColor:'blue',height:'800px'}}></div>
-      <div style={{backgroundColor:'green',height:'800px'}}></div>
-      <div style={{backgroundColor:'yellow',height:'800px'}}></div>
+      <>
+        <Location/>
+      </>
 
 
       <Footer/>
